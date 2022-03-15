1ad481e95813fcff4a8c7b18509f5e790b57974b
Łukasz Duda <lukas60055@gmail.com>
2022-03-15 23:08:21 +0100

    Update <head>. Added functions WordPress

--- header.php
+++ header.php
@@ -1,8 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+    <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('stylesheet_url'); ?>" />
+    <title><?php bloginfo('name'); ?></title>
+    <?php wp_head(); ?>
 </head>
