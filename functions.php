f17fbb53ea9cece6f4e7fb1300b5b4518e468a36
Łukasz Duda <lukas60055@gmail.com>
2022-03-17 19:04:11 +0100

    Added basic template pages and menu

--- functions.php
+++ functions.php
@@ -4,23 +4,62 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-add_theme_support(
-	'custom-logo',
-	[
-        'height'      => 60,
-        'width'       => 100,
-        'flex-width'  => true,
-        'flex-height' => true,
-	]
-);
+function theme_setup() {
+	register_nav_menus(
+		array(
+			'primary' => __('Top Menu'),
+		)
+	);
 
-function add_theme_menus() {
-	register_nav_menus(array(
-		'primary' => ('Top Menu'),
-	));
+	add_theme_support(
+		'custom-logo',
+		array(
+			'width'       => 250,
+			'height'      => 250,
+			'flex-width'  => true,
+			'flex-height' => true,
+		)
+	);
+
+	add_theme_support('editor-styles');
+
+	add_theme_support('wp-block-styles');
+
+	add_theme_support( 'responsive-embeds');
+
+	$starter_content = array(
+		'posts'       => array(
+			'home',
+			'about' => array(
+				'post_type' => 'page',
+				'post_title' => 'Gallery',
+				'post_content' =>'',
+			),
+			'contact',
+		),
+
+		'options'     => array(
+			'show_on_front'  => 'page',
+			'page_on_front'  => '{{home}}',
+		),
+
+		'nav_menus'   => array(
+			'primary'    => array(
+				'name'  => __('Top Menu'),
+				'items' => array(
+					'link_home',
+					'page_about',
+					'page_contact',
+				),
+			),
+		),
+	);
+	$starter_content = apply_filters('theme_starter_content', $starter_content);
+
+	add_theme_support('starter-content', $starter_content);
 }
+add_action('after_setup_theme', 'theme_setup');
 
-add_action('init', 'add_theme_menus');
 
 function theme_scripts() {
 	$theme_version = wp_get_theme()->get('Version');
