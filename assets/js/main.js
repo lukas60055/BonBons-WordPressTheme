c5ea05a56ba61932da8df8f292e68abcd2ea4a64
Łukasz Duda <lukas60055@gmail.com>
2022-03-17 21:24:59 +0100

    Display text in the gallery

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,4 +7,9 @@ jQuery(function ($) {
         $('body').toggleClass('menu-open');
     });
 
+
+    // Add text to Galleries
+    $('a[data-lightbox="post-image"]').each( (i, e) => {
+        $(e).attr('data-title', $(e).parent().children('figcaption').text());
+    });
 });
